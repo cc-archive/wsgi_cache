b46a60cf58748fb7307264f01b7c4e6fb75b9738
Nathan R. Yergler <nathan@yergler.net>
2010-07-19 16:58:49 -0700

    Allow test requests to specify additional response headers.

--- wsgi_cache/tests/__init__.py
+++ wsgi_cache/tests/__init__.py
@@ -10,7 +10,8 @@ TESTING_HEADERS = [('Content-Type', 'text/test')]
 def app(environ, start_response):
     """A dummy app that emits sentinel values we can use for testing."""
 
-    start_response(TESTING_STATUS, TESTING_HEADERS)
+    headers = TESTING_HEADERS[:] + environ.get('cc.headers', [])
+    start_response(TESTING_STATUS, headers)
 
     return [environ['contents']]
 
